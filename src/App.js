f03503f136eb9f2d4b9841db3b92ee55a073f8d6
Jesús Alexander Herrera Acevedo <152633538+ExplosiveSmile05@users.noreply.github.com>
2025-03-28 16:02:11 +0000

    Acrualizacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
+//importar imagenes y les asignamos un objeto
+import MarioImage from './imagenes/Mario.png'
+import LuigiImage from ',/imagenes/Luigi.png'
+import PeachImage from ',/imagenes/Peach.png'
+
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros</div>
-        <Tarjeta nombre="Mario" colorFavorito="Rojo"/>
-        <Tarjeta nombre="Luigi" colorFavorito="Verde"/>
-        <Tarjeta nombre="Peach" colorFavorito="Rosa"/>
+        <Tarjeta nombre="Mario" colorFavorito="Rojo" image={MarioImage}/>
+        <Tarjeta nombre="Luigi" colorFavorito="Verde" image={LuigiImage}/>
+        <Tarjeta nombre="Peach" colorFavorito="Rosa" image={PeachImage}/>
     </div>
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -3,8 +3,9 @@ import React from 'react'
 export function Tarjeta( props ) {
   return (
     <div>
+      <img src={props.image} width={150} />
       <div>Nombre del Personaje: {props.nombre}</div>
-      <div>color Favorito: {props.coloFavorito}</div>
+      <div>color Favorito: {props.colorFavorito}</div>
     </div>
   )
 }
